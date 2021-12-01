8888710465566f3332825793a8af0664fdc7fcb0
Florian <florian.abeillon@polytechnique.edu>
2021-12-01 18:13:24 +0100

    Reorganized files

--- code/src/display/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.display.display_agents import plot_avg_q_table, plot_epsilon
-from src.display.display_buyers import plot_avg_sub_q_tables
-from src.display.display_sellers import plot_prices
